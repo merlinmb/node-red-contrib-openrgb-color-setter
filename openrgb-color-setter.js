894a084c5ad16d5c4a4c92e6855d5fbc76d306da
merlinmb <markbeets@gmail.com>
2023-06-21 10:23:20 +0100

    new logic

--- openrgb-color-setter.js
+++ openrgb-color-setter.js
@@ -2,29 +2,27 @@ module.exports = function(RED) {
   // Required modules
   const OpenRGB = require('openrgb-sdk');
 
-  // Create an instance of OpenRGB
-  const openRGB = new OpenRGB();
-
-  // Initialize the connection to the OpenRGB server
-  openRGB.init();
-
-  // List of available devices
-  const deviceList = [
-    { name: 'Device 1', id: 1 },
-    { name: 'Device 2', id: 2 },
-    // Add more devices as needed
-  ];
-
   // Function to set the color of a device
-  function setDeviceColor(deviceId, color) {
-    const device = openRGB.getController(deviceId);
-    if (!device) {
-      console.error('Device not found');
-      return;
-    }
+  function setDeviceColor(server, port, deviceId, color) {
+    const openRGB = new OpenRGB({
+      server,
+      port
+    });
 
-    const [r, g, b] = hexToRgb(color);
-    device.setColor(0, r, g, b);
+    openRGB.init()
+      .then(() => {
+        const device = openRGB.getController(deviceId);
+        if (!device) {
+          console.error('Device not found');
+          return;
+        }
+
+        const [r, g, b] = hexToRgb(color);
+        device.setColor(0, r, g, b);
+      })
+      .catch((error) => {
+        console.error('OpenRGB initialization failed', error);
+      });
   }
 
   // Helper function to convert HEX color to RGB
@@ -41,6 +39,8 @@ module.exports = function(RED) {
     RED.nodes.createNode(this, config);
 
     const node = this;
+    const server = config.server;
+    const port = config.port;
 
     // Handle incoming messages
     this.on('input', function(msg) {
@@ -48,7 +48,7 @@ module.exports = function(RED) {
       const color = msg.payload;
 
       // Set the color of the selected device
-      setDeviceColor(selectedDevice, color);
+      setDeviceColor(server, port, selectedDevice, color);
 
       // Pass the original message to the next node
       node.send(msg);
@@ -60,6 +60,26 @@ module.exports = function(RED) {
 
   // Provide the list of devices as configuration options for the custom node
   RED.httpAdmin.get('/openrgb-devices', function(req, res) {
-    res.json(deviceList);
+    // Retrieve the server and port from the request query
+    const server = req.query.server || '';
+    const port = req.query.port || '';
+
+    // Create an instance of OpenRGB
+    const openRGB = new OpenRGB({server,port});
+
+    openRGB.init()
+      .then(() => {
+        // Fetch the device list
+        const devices = openRGB.getAllControllers().map((device) => ({
+          id: device.deviceId,
+          name: device.name,
+        }));
+
+        res.json(devices);
+      })
+      .catch((error) => {
+        console.error('OpenRGB initialization failed', error);
+        res.status(500).send('OpenRGB initialization failed');
+      });
   });
 };
